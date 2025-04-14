61c6583650f845f278bf927b9a3e6d7b7bb19ccb
axbg <bisagalexstefan@gmail.com>
2025-04-15 02:34:31 +0300

    Replace previous logos with codeberg

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,44 +1,8 @@
 <template>
   <footer class="footer -mx-4 md:mx-0">
     <div class="container mx-auto flex justify-center">
-      <div
-        class="container centered-container text-white text-2xl xl:text-3xl w-2/3 lg:w-1/3 flex justify-between"
-      >
-        <a
-          class="hover:text-red-700"
-          href="mailto:bisagalexstefan@gmail.com"
-          title="Gmail"
-          target="blank"
-        >
-          <fa :icon="faGoogle" />
-        </a>
-        <a
-          class="hover:text-blue-900"
-          href="https://www.messenger.com/t/bisagalex"
-          title="Facebook"
-          target="blank"
-        >
-          <fa :icon="faFacebookMessenger" />
-        </a>
-        <a class="transitioned" href="https://shadowed.digital" title="shadowed" target="blank">
-          <img src="/images/shadowed.png" alt="shadowed" />
-        </a>
-        <a class="hover:text-black" href="https://github.com/axbg" title="Github" target="blank">
-          <fa :icon="faGithub" />
-        </a>
-        <a
-          class="hover:text-blue-900"
-          href="https://www.linkedin.com/in/alexbisag/"
-          title="LinkedIn"
-          target="blank"
-        >
-          <fa :icon="faLinkedin" />
-        </a>
-      </div>
-    </div>
-    <div class="text-white text-center pb-1 pt-3 xl:pt-5">
-      <a class="text-xs powered-by" href="https://github.com/Gomah/bluise" target="blank">
-        Powered by Bluise
+      <a href="https://codeberg.org/axbg" title="codeberg" target="blank">
+        <img class="h-10 pb-2" src="/images/codeberg.png" alt="codeberg" />
       </a>
     </div>
   </footer>
@@ -46,31 +10,10 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import {
-  faFacebookMessenger,
-  faGoogle,
-  faLinkedin,
-  faGithub,
-  IconDefinition,
-} from '@fortawesome/free-brands-svg-icons';
 
 @Component
 export default class Footer extends Vue {
-  get faFacebookMessenger(): IconDefinition {
-    return faFacebookMessenger;
-  }
 
-  get faGoogle(): IconDefinition {
-    return faGoogle;
-  }
-
-  get faLinkedin(): IconDefinition {
-    return faLinkedin;
-  }
-
-  get faGithub(): IconDefinition {
-    return faGithub;
-  }
 }
 </script>
 
